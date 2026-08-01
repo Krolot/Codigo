e1c106d64c4de45c235dba48b47fccc2583f2a9d
Cayama0811 <krolot036@gmail.com>
2026-07-31 21:41:51 -0400

    PONGO 2: AHORA ES PERSONAL
    TIENE INTELIGENCIA ARTIFICIAL AVANZADA, MENU PRINCIPAL DE ALTA CALIDAD, MODO MULTIJUGADOR CON NETWORKING DE PROXIMA GENERACION, RAQUETAS DE PONG DE GRADO MILITAR, PELOTA DE PONG DE TITANIO, IA GENERATIVA STABLE DIFFUSION SUPER QUALITY PHOTOREAL

--- c++/Raylib/PongRaylib/main.cpp
+++ c++/Raylib/PongRaylib/main.cpp
@@ -3,8 +3,17 @@
 const int screenWidth = 800;
 const int screenHeight = 600;
 
+enum Scene
+{
+    MENU,
+    PLAYER_VS_PLAYER,
+    PLAYER_VS_AI
+};
+
+Scene currentScene = MENU;
+
 class CPongPlayer{
-    private:
+    protected:
     Vector2 pos;
     Vector2 size;
     Rectangle collisionBox;
@@ -81,6 +90,68 @@ int CPongPlayer::GetScore()
     return score;
 }
 
+class CPongEnemy : public CPongPlayer
+{
+    public:
+    enum Difficulty
+    {
+        EASY,
+        MEDIUM,
+        HARD
+    };
+
+    private:
+    Difficulty difficulty;
+
+    public:
+    CPongEnemy(Vector2 p, Vector2 si, int s, float speed);
+    void UpdateAI(Vector2 ballPos, Vector2 ballVelocity);
+    void SetDifficulty(Difficulty d);
+};
+
+CPongEnemy::CPongEnemy(Vector2 p, Vector2 si, int s, float sp) : CPongPlayer(p, si, s, sp)
+{
+    difficulty = EASY;
+}
+
+void CPongEnemy::UpdateAI(Vector2 ballPos, Vector2 ballVelocity)
+{
+    float dt = GetFrameTime();
+    float center = pos.y + size.y / 2;
+    float difference = ballPos.y - center;
+    float requiredDifference;
+    switch (difficulty)
+    {
+        case EASY:
+            requiredDifference = 100;
+            break;
+        case MEDIUM:
+            requiredDifference = 75;
+            break;
+        case HARD:
+            requiredDifference = 60;
+            break;
+    }
+
+    if (ballVelocity.x <= 0)
+        return;
+    if(difference > requiredDifference)
+    {
+        pos.y += speed * dt;
+    }
+    else if(difference < -requiredDifference)
+    {
+        pos.y -= speed * dt;
+    }
+        collisionBox.x = pos.x;
+        collisionBox.y = pos.y;
+}
+
+void CPongEnemy::SetDifficulty(Difficulty d)
+{
+    difficulty = d;
+}
+
 class CPongBall{
     private:
     Vector2 pos;
@@ -97,6 +168,7 @@ class CPongBall{
     void ResetPos(bool isLeft);
     void Collisions();
     void ModifyXDirection(bool isLeft);
+    Vector2 GetVelocity();
     Vector2 GetPos();
     Rectangle GetHitBox();
 };
@@ -163,6 +235,11 @@ Rectangle CPongBall::GetHitBox()
     return collisionBox;
 }
 
+Vector2 CPongBall::GetVelocity()
+{
+    return velocity;
+}
+
 void CPongBall::ResetPos(bool isLeft)
 {
     pos = {screenWidth / 2, screenHeight / 2};
@@ -180,10 +257,11 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-
     CPongPlayer Player1(Vector2 {50, screenHeight / 2}, Vector2{10, 100}, 0, 500.0f);
     CPongPlayer Player2(Vector2 {750, screenHeight / 2}, Vector2{10, 100}, 0, 500.0f);
-    CPongBall Ball(Vector2 {screenWidth / 2, screenHeight / 2}, Vector2{-1,-1}, 250, 15);
+    CPongEnemy PlayerAI(Vector2 {750, screenHeight / 2}, Vector2{10, 100}, 0, 500.0f);
+
+    CPongBall Ball(Vector2 {screenWidth / 2, screenHeight / 2}, Vector2{-1,-1}, 400.0f, 15);
 
     InitWindow(screenWidth, screenHeight, "Pongo");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -195,38 +273,100 @@ int main(void)
         // Update
         //----------------------------------------------------------------------------------
 
-        if (IsKeyDown(KEY_W))
-            Player1.Move(1);
-        if (IsKeyDown(KEY_S))
-            Player1.Move(0);
-            
-        if (IsKeyDown(KEY_UP))
-            Player2.Move(1);
-        if (IsKeyDown(KEY_DOWN))
-            Player2.Move(0);
-
-        if (CheckCollisionRecs(Player1.GetHitBox(), Ball.GetHitBox()))
-            Ball.ModifyXDirection(1);
-        if (CheckCollisionRecs(Player2.GetHitBox(), Ball.GetHitBox()))
-            Ball.ModifyXDirection(0);
-
-        if (Ball.GetPos().x < 0)
+        switch(currentScene)
         {
-            Player2.AddScore();
-            Ball.ResetPos(0);
-        }
+            case MENU:
+                if (IsKeyPressed(KEY_ONE))
+                    currentScene = PLAYER_VS_PLAYER;
+                if (IsKeyPressed(KEY_TWO))
+                    currentScene = PLAYER_VS_AI;
+                break;
+            case PLAYER_VS_PLAYER:
+                if (IsKeyDown(KEY_W))
+                    Player1.Move(1);
+                if (IsKeyDown(KEY_S))
+                    Player1.Move(0);
+
+                if (IsKeyDown(KEY_UP))
+                    Player2.Move(1);
+                if (IsKeyDown(KEY_DOWN))
+                    Player2.Move(0);
             
-        if (Ball.GetPos().x > screenWidth)
-        {
-            Player1.AddScore();
-            Ball.ResetPos(1);
-        }    
+                if (CheckCollisionRecs(Player1.GetHitBox(), Ball.GetHitBox()))
+                    Ball.ModifyXDirection(1);
 
-        Ball.Move();
+                if (CheckCollisionRecs(Player2.GetHitBox(), Ball.GetHitBox()))
+                    Ball.ModifyXDirection(0);
 
-        Player1.Collisions();
-        Player2.Collisions();
-        Ball.Collisions();
+                if (Ball.GetPos().x < 0)
+                {
+                    Player2.AddScore();
+                    Ball.ResetPos(0);
+                }
+                    
+                if (Ball.GetPos().x > screenWidth)
+                {
+                    Player1.AddScore();
+                    Ball.ResetPos(1);
+                }
+
+                Ball.Move();
+                Player1.Collisions();
+                Player2.Collisions();
+                Ball.Collisions();
+
+                break;
+            case PLAYER_VS_AI:
+                if (IsKeyDown(KEY_W))
+                    Player1.Move(1);
+                if (IsKeyDown(KEY_S))
+                    Player1.Move(0);
+
+                if (IsKeyDown(KEY_UP))
+                    PlayerAI.Move(1);
+                if (IsKeyDown(KEY_DOWN))
+                    PlayerAI.Move(0);
+            
+                if (CheckCollisionRecs(Player1.GetHitBox(), Ball.GetHitBox()))
+                    Ball.ModifyXDirection(1);
+
+                if (CheckCollisionRecs(PlayerAI.GetHitBox(), Ball.GetHitBox()))
+                    Ball.ModifyXDirection(0);
+
+                if (Ball.GetPos().x < 0)
+                {
+                    PlayerAI.AddScore();
+                    Ball.ResetPos(0);
+                }
+                    
+                if (Ball.GetPos().x > screenWidth)
+                {
+                    Player1.AddScore();
+                    Ball.ResetPos(1);
+                }
+                
+                PlayerAI.UpdateAI(Ball.GetPos(), Ball.GetVelocity());    
+                int scoreDifference = Player1.GetScore() - PlayerAI.GetScore();
+
+                if (scoreDifference >= 5)
+                {
+                    PlayerAI.SetDifficulty(CPongEnemy::HARD);
+                }
+                else if (scoreDifference <= -5)
+                {
+                    PlayerAI.SetDifficulty(CPongEnemy::EASY);
+                }
+                else
+                {
+                    PlayerAI.SetDifficulty(CPongEnemy::MEDIUM);
+                }
+
+                Ball.Move();
+                Player1.Collisions();
+                PlayerAI.Collisions();
+                Ball.Collisions();
+                break;
+            }
         
         //----------------------------------------------------------------------------------
         // Draw
@@ -234,13 +374,33 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            Player1.Draw();
-            Player2.Draw();
-            Ball.Draw();
+
+            switch(currentScene)
+            {
+                case MENU:
+                    DrawText("Pongo", screenWidth / 2 - 50, screenHeight / 2 - 100, 40, MAROON);
+                    DrawText("Press 1 for Player vs Player", screenWidth / 2 - 150, screenHeight / 2 - 20, 20, MAROON);
+                    DrawText("Press 2 for Player vs AI", screenWidth / 2 - 150, screenHeight / 2 + 20, 20, MAROON);
+                    break;
+                case PLAYER_VS_PLAYER:
+                    Player1.Draw();
+                    Player2.Draw();
+                    Ball.Draw();
 
             DrawText(TextFormat("%0i", Player1.GetScore()), screenWidth / 2 - 50, 20, 30, MAROON);
             DrawText(TextFormat("%0i", Player2.GetScore()), screenWidth / 2 + 50, 20, 30, MAROON);
 
+                    break;
+                case PLAYER_VS_AI:
+                    Player1.Draw();
+                    PlayerAI.Draw();
+                    Ball.Draw();
+
+                    DrawText(TextFormat("%0i", Player1.GetScore()), screenWidth / 2 - 50, 20, 30, MAROON);
+                    DrawText(TextFormat("%0i", PlayerAI.GetScore()), screenWidth / 2 + 50, 20, 30, MAROON);
+
+                    break;
+            }
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
